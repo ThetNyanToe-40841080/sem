678fb033693f76bf54774b469609c04570394ab0
ide-dev-05 <yaircube1@gmail.com>
2026-09-25 12:53:32 +0630

    complete get all salaries along with version no. changing

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,10 @@
-# Stage 1: Build the Java application using Maven
 FROM maven:3.9.6-eclipse-temurin-17 AS builder
 WORKDIR /tmp
 COPY pom.xml .
 COPY src ./src
 RUN mvn clean package -DskipTests
 
-# Stage 2: Create the lightweight runtime image
 FROM eclipse-temurin:25
 WORKDIR /tmp
-COPY --from=builder /tmp/target/seMethods-1.0-SNAPSHOT-jar-with-dependencies.jar /tmp/seMethods.jar
+COPY --from=builder /tmp/target/seMethods-0.1.0.4-jar-with-dependencies.jar /tmp/seMethods.jar
 ENTRYPOINT ["java", "-jar", "seMethods.jar", "db:3306"]
--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -187,7 +187,19 @@ public class App
             return null;
         }
     }
-
+    public void printSalaries(ArrayList<Employee> employees)
+    {
+        // Print header
+        System.out.println(String.format("%-10s %-15s %-20s %-8s", "Emp No", "First Name", "Last Name", "Salary"));
+        // Loop over all employees in the list
+        for (Employee emp : employees)
+        {
+            String emp_string =
+                    String.format("%-10s %-15s %-20s %-8s",
+                            emp.emp_no, emp.first_name, emp.last_name, emp.salary);
+            System.out.println(emp_string);
+        }
+    }
     public void displayEmployee(Employee emp)
     {
         if (emp != null)
@@ -238,8 +250,7 @@ public class App
 
         ArrayList<Employee> allemployees = a.getAllSalaries();
 
-        // Test the size of the returned data - should be 240124
-        System.out.println(allemployees.size());
+        a.printSalaries(allemployees);
 
         a.disconnect();
     }
