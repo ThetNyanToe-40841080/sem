585e802fa3f2ebf1893ab96a65dded0f9a869cf4
ide-dev-05 <yaircube1@gmail.com>
2026-09-25 12:39:03 +0630

    part 1 of get all salaries

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -153,6 +153,41 @@ public class App
         }
     }
 
+    public ArrayList<Employee> getAllSalaries()
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT employees.emp_no, employees.first_name, employees.last_name, salaries.salary "
+                            + "FROM employees, salaries "
+                            + "WHERE employees.emp_no = salaries.emp_no AND salaries.to_date = '9999-01-01' "
+                            + "ORDER BY employees.emp_no ASC";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract employee information
+            ArrayList<Employee> employees = new ArrayList<Employee>();
+            while (rset.next())
+            {
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("employees.emp_no");
+                emp.first_name = rset.getString("employees.first_name");
+                emp.last_name = rset.getString("employees.last_name");
+                emp.salary = rset.getInt("salaries.salary");
+                employees.add(emp);
+            }
+            return employees;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get salary details");
+            return null;
+        }
+    }
+
     public void displayEmployee(Employee emp)
     {
         if (emp != null)
@@ -180,6 +215,7 @@ public class App
         }
     }
 
+
     public static void main(String[] args)
     {
         App a = new App();
@@ -199,6 +235,11 @@ public class App
                 a.getSalariesByRole("Engineer");
 
         a.displaySalariesByRole(employees);
+
+        ArrayList<Employee> allemployees = a.getAllSalaries();
+
+        // Test the size of the returned data - should be 240124
+        System.out.println(allemployees.size());
 
         a.disconnect();
     }
